23eb31fda6b6e62dddcc99a284527cb42c2d8a73
Atn010 <>
2018-05-08 14:32:49 +0700

    First Commit

---
+++ Money Board/AdditionVC.swift
@@ -0,0 +1,35 @@
+//
+//  AdditionVC.swift
+//  Money Board
+//
+//  Created by Antonius George on 08/05/18.
+//  Copyright © 2018 Apple Developer Academy @ Binus. All rights reserved.
+//
+
+import UIKit
+
+class AdditionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Money Board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Money Board
+//
+//  Created by Antonius George on 08/05/18.
+//  Copyright © 2018 Apple Developer Academy @ Binus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
